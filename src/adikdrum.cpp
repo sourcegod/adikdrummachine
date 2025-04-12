f21a76f9aa33d3be3351205ed357a3bc08c543fa
Coolbrother <coolbrother@sourcegod.com>
2025-04-12 23:43:39 +0200

    Updating: adding (8 new sounds in drumSounds vector)

--- src/adikdrum.cpp
+++ src/adikdrum.cpp
@@ -16,26 +16,29 @@
 #include <termios.h>
 #include <unistd.h>
 #include "audiodriver.h" // Inclure le header de AudioDriver
-#include "soundgenerator.h" // Inclure le header de SoundGenerator
 #include "soundfactory.h" // Inclure le header de SoundFactory
 #include "drumplayer.h"
 
 const double PI = 3.14159265358979323846;
+const int NUM_SOUNDS = 16; // Notre constante globale pour le nombre de sons
 
 // Mapping des touches et des sons
+
 std::map<char, int> keyToSoundMap = {
     {'q', 0}, {'s', 1}, {'d', 2}, {'f', 3},
-    {'g', 4}, {'h', 5}, {'j', 6}, {'k', 7}
+    {'g', 4}, {'h', 5}, {'j', 6}, {'k', 7},
+    {'a', 8}, {'z', 9}, {'e', 10}, {'r', 11},
+    {'t', 12}, {'y', 13}, {'u', 14}, {'i', 15}
 };
 
 struct DrumMachineData {
     std::vector<std::vector<double>> sounds;
-    std::vector<double>::iterator currentSound[8];
+    std::vector<double>::iterator currentSound[NUM_SOUNDS]; // Utilisation de la constante
     DrumPlayer player;
     int sampleRate;
-    //
+
     // Ajoute ce constructeur !
-    DrumMachineData() : player(8) {}
+    DrumMachineData() : player(NUM_SOUNDS), sounds(NUM_SOUNDS) {} // Utilisation de la constante ici aussi
 };
 
 
@@ -93,7 +96,7 @@ int main() {
         int sampleRate = 44100;
         double defaultDuration = 0.5;
         SoundFactory soundFactory(sampleRate, defaultDuration);
-        std::vector<std::vector<double>> drumSounds(8);
+        std::vector<std::vector<double>> drumSounds(NUM_SOUNDS);
         // ... (génération des sons) ...
         drumSounds[0] = soundFactory.generateKick();
         drumSounds[1] = soundFactory.generateSnare();
@@ -103,16 +106,23 @@ int main() {
         drumSounds[5] = soundFactory.generateCymbal(3.0);
         drumSounds[6] = soundFactory.generateTestTone(440.0);
         drumSounds[7] = soundFactory.generateTestTone(550.0);
+        drumSounds[8] = soundFactory.generateTestTone(220.0); // Exemple pour RimShot
+        drumSounds[9] = soundFactory.generateTestTone(330.0); // Exemple pour HandClap
+        drumSounds[10] = soundFactory.generateHiHat(0.5); // Exemple pour HiHatOpen
+        drumSounds[11] = soundFactory.generateTestTone(110.0); // Exemple pour LowTom
+        drumSounds[12] = soundFactory.generateTestTone(165.0); // Exemple pour MidTom
+        drumSounds[13] = soundFactory.generateTestTone(275.0); // Exemple pour HiTom
+        drumSounds[14] = soundFactory.generateTestTone(660.0); // Exemple pour CowBell
+        drumSounds[15] = soundFactory.generateTestTone(880.0); // Exemple pour Tambourine
+
 
         DrumMachineData drumData;
         drumData.sounds = drumSounds;
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < NUM_SOUNDS; ++i) {
             drumData.currentSound[i] = drumData.sounds[i].begin();
         }
-        drumData.player = DrumPlayer(8); // Initialiser DrumPlayer avec le nombre de sons
+        drumData.player = DrumPlayer(NUM_SOUNDS); // Initialiser DrumPlayer avec le nombre de sons
         drumData.sampleRate = sampleRate;
-        //
-        // Plus besoin de créer outputParameters ici !
 
         err = audioDriver.openStream(sampleRate, drumMachineCallback, &drumData);
         if (err != paNoError) {
@@ -126,7 +136,7 @@ int main() {
         }
 
         std::cout << "Test des sons refactorisés avec AudioDriver..." << std::endl;
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < NUM_SOUNDS; ++i) {
             drumData.player.triggerSound(drumData.sounds, drumData.currentSound, i);
             Pa_Sleep(static_cast<int>(drumData.sounds[i].size() * 1000.0 / sampleRate * 0.8));
         }
