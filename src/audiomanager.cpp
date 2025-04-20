28e25ea46dcafdc3e7e4e612a1410942bca1daa5
Coolbrother <coolbrother@sourcegod.com>
2025-04-20 15:37:22 +0200

    Adding files: (audiomanager.h, .cpp)

---
+++ src/audiomanager.cpp
@@ -0,0 +1,35 @@
+#include "audiomanager.h"
+#include <iostream>
+#include "audiodriver.h" // Assure-toi d'inclure ton AudioDriver
+
+AudioManager::AudioManager() {
+}
+
+AudioManager::~AudioManager() {
+    close();
+}
+
+bool AudioManager::init(PaStreamCallback *callback, void *userData, int sampleRate, int framesPerBuffer) {
+
+    if (audioDriver_.init(callback, userData, sampleRate, framesPerBuffer) == paNoError) {
+        std::cout << "Audio driver initialized successfully." << std::endl;
+        if (audioDriver_.start() == paNoError) {
+            std::cout << "Audio stream started successfully." << std::endl;
+            return true;
+        } else {
+            std::cerr << "Failed to start audio stream: " << Pa_GetErrorText(audioDriver_.getLastError()) << std::endl;
+            audioDriver_.close();
+            return false;
+        }
+    } else {
+        std::cerr << "Hey man, Failed to initialize audio driver: " << Pa_GetErrorText(audioDriver_.getLastError()) << std::endl;
+        return false;
+    }
+}
+
+void AudioManager::close() {
+    audioDriver_.stop();
+    audioDriver_.close();
+    std::cout << "Audio driver stopped and terminated." << std::endl;
+}
+
---
+++ src/audiomanager.h
@@ -0,0 +1,19 @@
+#ifndef AUDIOMANAGER_H
+#define AUDIOMANAGER_H
+
+#include "audiodriver.h"
+#include <portaudio.h> // Assure-toi d'inclure portaudio.h si AudioDriver l'utilise directement
+
+class AudioManager {
+public:
+    AudioManager();
+    ~AudioManager();
+
+    bool init(PaStreamCallback *callback, void *userData, int sampleRate, int framesPerBuffer);
+    void close();
+
+private:
+    AudioDriver audioDriver_;
+};
+
+#endif // AUDIOMANAGER_H
